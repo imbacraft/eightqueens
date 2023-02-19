e63ce47b4b4764ef05557ac71682d92ff5c963f2
Dimitris Bouzoukas <dimbouzoukas@gmail.com>
2023-02-19 14:05:26 +0200

    Instructions

--- src/main/java/eightqueens/App.java
+++ src/main/java/eightqueens/App.java
@@ -1,12 +1,38 @@
 package eightqueens;
 
+import java.util.Arrays;
+
 /**
  * OBJECTIVE:
  * Place 8 queens on a chessboard in such way that none of the queens can take each other.
  *
+ * Chessboard has 8x8 tiles.
+ *
+ * The program must print a two dimensional array  where 1 means the queen is there, 0 means the queen is not there.
+ *
+ * End result looks like this:
+ * 0 0 1 0 0 0 0 0
+ * 0 0 0 0 1 0 0 0
+ * 0 0 0 0 0 0 1 0
+ * 0 1 0 0 0 0 0 0
+ * 0 0 0 1 0 0 0 0
+ * 0 0 0 0 0 1 0 0
+ * 0 0 0 0 0 0 0 1
+ * 0 0 0 0 0 0 0 0
+ *
+ *
  */
 public class App  {
+
     public static void main( String[] args ) {
 
+        int[][] chessboard = new int[8][2];
+
+        chessboard[0][0] = 1;
+
+
+
+
     }
+
 }
